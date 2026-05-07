eac297f6b1fb4c5c0e18492d463a43de60bdf9ff
Kasper Kjersgaard Mortensen <kasper@rasmor.dk>
2026-05-07 09:56:55 +0200

    Made functions to tidy and pivot_longer survey data and moved it to functions.R and added it to cleaning

--- R/functions.R
+++ R/functions.R
@@ -21,7 +21,7 @@ read <- function(file_path, max_rows = 100) {
 #'
 #' @returns Returns a dataframe including all data of the specific type
 read_all <- function(filename) {
-  files <- here("data-raw/nurses-stress/") |>
+  files <- here::here("data-raw/nurses-stress/") |>
     fs::dir_ls(regexp = filename, recurse = TRUE)
   data <- files |>
     purrr::map(read) |>
@@ -55,20 +55,78 @@ get_participant_id <- function(data) {
 #'
 #' @returns summarised data by id and collection_datetime (minutes)
 
-summarise_by_datetime <- function(data) {
+summarise_by_datetime <- function(data, fns, unit) {
   summarised_data <- data |>
     dplyr::mutate(
       collection_datetime = lubridate::round_date(
         collection_datetime,
-        unit = "minute"
+        unit = unit
       )
     ) |>
     dplyr::summarise(
       dplyr::across(
         tidyselect::where(is.numeric),
-        list(mean = mean, sd = sd, median = median)
+        fns
       ),
       .by = c(id, collection_datetime)
     )
   return(summarised_data)
+}
+
+
+# modify to be able to change unit
+#' Read_sensor_data
+#'
+#' @param filename name of file
+#' @param unit unit of datetime
+#'
+#' @returns dataframe that has id and is summarised by datetime
+
+read_sensor_data <- function(filename, fns, unit) {
+  data <- read_all(filename) |>
+    get_participant_id() |>
+    summarise_by_datetime(unit = unit, fns=fns)
+  return(data)
+}
+
+#' tidy_survey_dates
+#'
+#' @param data dataframe survey_data
+#'
+#' @returns a tidy dataframe
+tidy_survey_dates <- function(data) {
+  tidied <- data |>
+    dplyr::mutate(
+      date = lubridate::mdy(date),
+      start_datetime = lubridate::as_datetime(paste(date, start_time)),
+      end_datetime = lubridate::as_datetime(paste(date, end_time)),
+      datetime_id = start_datetime,
+      .before = start_time
+    ) |>
+    dplyr::select(-c(date, start_time, end_time, duration))
+  return(tidied)
+}
+
+#' survey_to_long
+#'
+#' @param data tidy dataframe on survey
+#'
+#' @returns survey data in a longer format dataframe
+survey_to_long <- function(data) {
+  longer <- data |>
+    dplyr::select(id, datetime_id, start_datetime, end_datetime) |>
+    tidyr::pivot_longer(c(start_datetime, end_datetime),
+                        names_to = NULL,
+                        values_to = "collection_datetime"
+    ) |>
+    dplyr::group_by(dplyr::pick(-collection_datetime)) |>
+    tidyr::complete(
+      collection_datetime = seq(
+        min(collection_datetime),
+        max(collection_datetime),
+        by = 60
+      )
+    ) |>
+    dplyr::ungroup()
+  return(longer)
 }
